39e9f336bc3ece40392f01f2b55f3af1936db9de
deeepikaaa91 <deeepikaaa91@gmail.com>
2025-05-09 15:47:07 +0530

    crmcodex

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HashRouter, Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </HashRouter>
+  )
+}
+
+export default App
